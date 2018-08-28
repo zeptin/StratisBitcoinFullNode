861f1e180b84a58a5c2701c4eb26cee7a426951f
Kevin Loubser <zeptin@gmail.com>
2018-08-28 22:46:29 +0200

    Add minimal model for decoderawtransaction API

---
+++ src/Stratis.Bitcoin.Features.Wallet/Models/DecodeRawTransactionModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using NBitcoin;
+using Newtonsoft.Json;
+
+namespace Stratis.Bitcoin.Features.Wallet.Models
+{
+    /// <summary>
+    /// </summary>
+    public class DecodeRawTransactionModel
+    {
+        /// <summary>
+        /// The transaction in hex format.
+        /// </summary>
+        [JsonProperty(PropertyName = "rawHex")]
+        public string RawHex { get; set; }
+    }
+}
