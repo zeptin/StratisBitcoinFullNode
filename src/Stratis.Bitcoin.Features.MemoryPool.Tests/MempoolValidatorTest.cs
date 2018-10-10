e652760f535bfa774e720265a3a3076d0823780a
Kevin Loubser <zeptin@gmail.com>
2018-10-10 23:01:36 +0200

    Absurd fee

--- src/Stratis.Bitcoin.Features.MemoryPool.Tests/MempoolValidatorTest.cs
+++ src/Stratis.Bitcoin.Features.MemoryPool.Tests/MempoolValidatorTest.cs
@@ -733,11 +733,33 @@ namespace Stratis.Bitcoin.Features.MemoryPool.Tests
             // - Insufficient priority for free transaction
         }
 
-        [Fact(Skip = "Not implemented yet.")]
-        public void AcceptToMemoryPool_TxFeeInvalidAbsurdlyHighFee_ReturnsFalse()
+        [Fact]
+        public async void AcceptToMemoryPool_TxFeeInvalidAbsurdlyHighFee_ReturnsFalseAsync()
         {
-            // TODO: Execute failure case for CheckFee
-            // - Absurdly high fee
+            string dataDir = GetTestDirectoryPath(this);
+
+            // Have to be on mainnet for this or the RequireStandard flag is not set in the mempool settings.
+            Network network = KnownNetworks.Main;
+            var minerSecret = new BitcoinSecret(new Key(), network);
+            ITestChainContext context = await TestChainFactory.CreateAsync(network, minerSecret.PubKey.Hash.ScriptPubKey, dataDir);
+            IMempoolValidator validator = context.MempoolValidator;
+            Assert.NotNull(validator);
+
+            var destSecret = new BitcoinSecret(new Key(), network);
+            var tx = new Transaction();
+            tx.AddInput(new TxIn(new OutPoint(context.SrcTxs[0].GetHash(), 0), PayToPubkeyHashTemplate.Instance.GenerateScriptPubKey(minerSecret.PubKey)));
+            tx.AddOutput(new TxOut(context.SrcTxs[0].Outputs[0].Value - Money.Cents(50), destSecret.PubKeyHash));
+            tx.Sign(network, minerSecret, false);
+
+            var state = new MempoolValidationState(false);
+
+            // Force a non-zero absurd fee, even though the mempool is empty and hasn't gathered fee data.
+            state.AbsurdFee = Money.Cents(10);
+
+            // Tests the absurdly high fee case in CheckFee
+            bool isSuccess = await validator.AcceptToMemoryPool(state, tx);
+            Assert.False(isSuccess, "Transaction with fee too high should not have been accepted.");
+            Assert.Equal(MempoolErrors.AbsurdlyHighFee, state.Error);
         }
 
         [Fact(Skip = "Not implemented yet.")]
